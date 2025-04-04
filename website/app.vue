f92fbbde75af13493fa2ba38693bb2b118bab4f6
Dannon Baker <dannon.baker@gmail.com>
2025-04-03 20:25:58 -0400

    wrap app in uapp

--- website/app.vue
+++ website/app.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="h-dvh flex flex-col bg-chicago-50 font-sans">
-        <IWCHeader />
-        <main class="flex overflow-y-auto">
-            <NuxtPage />
-        </main>
-    </div>
+    <UApp>
+        <div class="h-dvh flex flex-col bg-chicago-50 font-sans">
+            <IWCHeader />
+            <main class="flex overflow-y-auto">
+                <NuxtPage />
+            </main>
+        </div>
+    </UApp>
 </template>
